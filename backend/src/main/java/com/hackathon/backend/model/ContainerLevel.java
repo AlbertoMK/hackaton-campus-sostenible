51c1dbbd7e498adc780239a6ff0e3f0d0089c692
Alberto Mozos <amozosk@gmail.com>
2025-05-28 10:17:39 +0200

    Añadida llamada a api

---
+++ backend/src/main/java/com/hackathon/backend/model/ContainerLevel.java
@@ -0,0 +1,13 @@
+package com.hackathon.backend.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ContainerLevel {
+    private String id;
+    private int level;
+}
--- backend/src/main/java/com/hackathon/backend/rest/ContainerRest.java
+++ backend/src/main/java/com/hackathon/backend/rest/ContainerRest.java
@@ -3,16 +3,38 @@ package com.hackathon.backend.rest;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hackathon.backend.model.Container;
+import com.hackathon.backend.model.ContainerLevel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
 public class ContainerRest {
+
+    @GetMapping("/api")
+    public ResponseEntity<String> getLevels() {
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                      .uri(URI.create("https://hackaton-campus-sostenible-api.mmartinez-d6a.workers.dev/containers/measurements"))
+                      .header("Authorization", "Bearer campus-sostenible-2025")
+                      .GET()
+                      .build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return ResponseEntity.ok(response.body());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().body(e.getMessage());
+        }
+    }
 
     @GetMapping("/containers")
     public ResponseEntity<List<Container>> getContainers () {
@@ -26,5 +48,10 @@ public class ContainerRest {
             System.err.println(ex);
             return ResponseEntity.internalServerError().body(List.of());
         }
+    }
+
+    @GetMapping("/level")
+    public ResponseEntity<List<ContainerLevel>> getContainersLevel() {
+        return null;
     }
 }
