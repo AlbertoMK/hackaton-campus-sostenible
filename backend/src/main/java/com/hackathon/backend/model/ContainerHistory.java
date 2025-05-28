77a8fcb2168dd9b355ea15cda384ea228879e222
César Jiménez <cesar.jimenez@alumnos.upm.es>
2025-05-28 10:01:42 +0200

    Añadir clases modelo

---
+++ backend/src/main/java/com/hackathon/backend/model/ContainerHistory.java
@@ -0,0 +1,8 @@
+package com.hackathon.backend.model;
+
+import java.util.List;
+
+public class ContainerHistory {
+    private String id;
+    private List<LevelData> history;
+}
---
+++ backend/src/main/java/com/hackathon/backend/model/LevelData.java
@@ -0,0 +1,9 @@
+package com.hackathon.backend.model;
+
+import java.time.Instant;
+
+public class LevelData {
+
+    private Instant timestamp;
+    private int percentage;
+}
